4aaf3b70b73fdc77a96fa9927a55bea925d67cc9
micoliser <micoliser@gmail.com>
2023-05-04 08:15:22 +0100

    rewrote web static setup with puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,65 @@
+# This script sets up webservers for the deployment of the webstatic
+
+# Update and install nginx if it doesnt exist
+exec {'update':
+  provider => shell,
+  command  => 'apt-get -y update',
+}
+
+package {'nginx':
+  ensure   => installed,
+  provider => 'apt',
+}
+
+# create folders if they don't exist exits
+file {'test':
+  ensure => directory,
+  path   => '/data/web_static/releases/test/',
+}
+
+file {'shared':
+  ensure => directory,
+  path   => '/data/web_static/shared/',
+}
+
+# create an html file with fake content to test configuration
+exec {'index':
+  provider => shell,
+  command  => 'echo -e "<html>\\n\\t<head>\\n\\t</head>\\n\\t<body>\\n\\t\\t<h1>Hello ALX</h1>\\n\\t</body>\\n</html>" > /data/web_static/releases/test/index.html',
+}
+
+# remove the symbolic link if exist and recreate it
+file {'delete-link':
+  ensure => absent,
+  path   => '/data/web_static/current',
+}
+
+file {'create-link':
+  ensure => link,
+  path   => '/data/web_static/current',
+  target => '/data/web_static/releases/test/',
+}
+
+# give ownership to the user and group ubuntu
+exec {'ownership':
+  provider => shell,
+  command  => 'chown -R ubuntu:ubuntu /data/',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# update the nginx config the content of /data/web_static/current/ to hbnb_static
+exec {'update-config':
+  provider => shell,
+  command  =>'sed -i "s/^\\s*location \\/ {/\\tlocation \\/hbnb_static {\\n\\t\\talias \\/data\\/web_static\\/current\\/;\\n\\t}\\n\\n&/" /etc/nginx/sites-enabled/default'
+
+# restart the server
+exec {'restart':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
+  subscribe   => Service['nginx'],
+}
