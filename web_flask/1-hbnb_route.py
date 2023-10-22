8208693f101a546bda664b57d7ea2f15732fcd7a
opxzy22 <aopeyem84@gmail.com>
2023-10-22 18:26:35 +0000

    done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" import Flask module from flask
+python script that start web application
+it listen on 0.0.0.0 to port 5000
+"""
 from flask import Flask
 
 app = Flask(__name__)
