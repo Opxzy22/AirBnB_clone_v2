ccbcc5b1da1e1d860b16fb9ee521241925a46945
opxzy22 <aopeyem84@gmail.com>
2023-10-23 08:22:04 +0000

    followed by the value of the text variablePython display

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def display_text(text):
     return "C {}" .format(text.replace('_', ' '))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route("/python/(<text>)", strict_slashes=False)
 def display_python(text="is cool"):
     return "Python {}" .format(text.replace('_', ' '))
