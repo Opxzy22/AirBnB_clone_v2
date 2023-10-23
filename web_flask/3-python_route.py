75f771bad8942ea31d5f69ab617445b62a4c1f92
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-10-23 14:40:59 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def display_text(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/(<text>)", strict_slashes=False)
 def display_python(text="is cool"):
+    """display “Python ”, followed by the value of the text variable"""
     return "Python {}".format(text.replace('_', ' '))
 
 
