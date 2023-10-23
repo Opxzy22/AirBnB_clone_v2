c9659c12fcb8b9f8054b77a206ce4e40a4fbb3e4
opxzy22 <aopeyem84@gmail.com>
2023-10-23 13:06:33 +0000

    progress

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,10 +33,11 @@ def display_number(n):
     return "{} is a number".format(n)
 
 
-@app.route("number_template/<int:n>", strict_slashes=False)
-def num_temp(n):
+@app.route("/number_template/int:<n>", strict_slashes=False)
+def num_temlate(n):
     return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
